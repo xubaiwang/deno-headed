0f14b9635d1430b43b135849d47301b5fae57651
王虚白 <18016038327@189.cn>
2025-05-16 11:47:11 +0800

    chore(nix): add Nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Headed fork of Deno";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell rec {
+            packages = [
+              rust-analyzer
+              rustfmt
+              clippy
+              vulkan-tools
+            ];
+            buildInputs = [
+              vulkan-loader
+              wayland 
+              alsa-lib
+              udev
+              libxkbcommon
+            ];
+            nativeBuildInputs = [
+              rustc
+              cargo
+              pkg-config
+              rustPlatform.bindgenHook
+              yq
+              cmake
+              protobuf
+            ];
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          };
+      }
+    );
+}
